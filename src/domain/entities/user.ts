97236c9dd504621cb7df4fa82c63595c68fbdb28
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-02-26 22:16:35 -0300

    chore: create user schema validation

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -1,14 +1,15 @@
 
 export class User {
-    private name!: string;
-    private email!: string;
-    private password!: string;
-    private id!: string;
+    private name: string;
+    private email: string;
+    private password: string;
+    private id: string;
 
-    constructor(name: string, email: string, password: string){
+    constructor(name: string, email: string, password: string, id: string){
         this.name = name;
         this.email = email;
         this.password = password;
+        this.id = id;
     }
     
     public getName(): string{
---
+++ src/validation/schema/createUserSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  name: z
+    .string()
+    .min(3, "Name must have more than 3 characteres!")
+    .max(15, "Name must have less than 15 characteres!"),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(5, "Password must have more than 5 characteres!")
+    .max(20, "Password must have less than 20 characteres!"),
+});
