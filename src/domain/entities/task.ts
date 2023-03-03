59a09b7ed4c1b47b3d455ce4165a34f514d112c9
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-03-03 18:48:28 -0300

    feat: task entity

---
+++ src/domain/entities/task.ts
@@ -0,0 +1,70 @@
+import { randomUUID } from "crypto";
+
+type data = {
+  title: string;
+  description: string;
+  done?: boolean;
+  projectId: string;
+  assignedTo?: string;
+  id?: string;
+};
+
+export class Task {
+  private id: string;
+  private title: string;
+  private description: string;
+  private done: boolean;
+  private assignedTo: string | undefined;
+  private projectId: string;
+
+  constructor({ description, projectId, title, assignedTo, done, id }: data) {
+    this.title = title;
+    this.description = description;
+    this.projectId = projectId;
+    this.assignedTo = assignedTo;
+    this.done = done ?? false;
+    this.id = id ?? randomUUID();
+  }
+
+  public get getTitle() {
+    return this.title;
+  }
+
+  public get getId() {
+    return this.id;
+  }
+
+  public get getDescription() {
+    return this.description;
+  }
+
+  public get getDone() {
+    return this.done;
+  }
+
+  public get getProjectId() {
+    return this.projectId;
+  }
+
+  public get getAssignedTo() {
+    return this.assignedTo;
+  }
+
+  public set setTitle(title: string) {
+    this.title = title;
+  }
+
+  public set setDescription(desc: string) {
+    this.description = desc;
+  }
+
+  public set setDone(done: boolean) {
+    this.done = done;
+  }
+
+  public set setAssignedTo(userId: string) {
+    if (this.assignedTo)
+      throw new Error("task has already been assigned to another user!");
+    this.assignedTo = userId;
+  }
+}
