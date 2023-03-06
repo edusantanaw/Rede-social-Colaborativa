b5a9c865938d61f4898ab853453bffe9f4f4538a
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-03-06 13:14:43 -0300

    chore: add cors dependece, and enable cors

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,23 +1,25 @@
-import express from 'express';
-import routes from './config/routes';
+import express from "express";
+import routes from "./config/routes";
+import cors from "cors";
 
 export class Server {
-    private app = express();
-    private PORT: number = 3000 
+  private app = express();
+  private PORT: number = 3000;
 
-    private middlewares(){
-        this.app.use(express.urlencoded({extended: true}));
-        this.app.use(express.json());
-    }
+  private middlewares() {
+    this.app.use(cors({ credentials: true, origin: "*" }));
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
 
-    private start(){
-        const cb = () => console.log(`Server running at ${this.PORT}`)
-        this.app.listen(this.PORT, cb);    
-    }
+  private start() {
+    const cb = () => console.log(`Server running at ${this.PORT}`);
+    this.app.listen(this.PORT, cb);
+  }
 
-    public bootstrap(){
-        this.middlewares();
-        routes(this.app);
-        this.start();
-    }
+  public bootstrap() {
+    this.middlewares();
+    routes(this.app);
+    this.start();
+  }
 }
