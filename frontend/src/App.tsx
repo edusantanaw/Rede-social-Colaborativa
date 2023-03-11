0f4062c539750397414441af97489d0f2fc64bca
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-03-11 11:37:13 -0300

    chore: react-router-dom configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,30 @@
 import { GlobalStyle } from "./styles/Global";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Auth from "./pages/auth/Auth";
+import Home from "./pages/home/Home";
 
 function App() {
+  const auth = false;
   return (
     <div className="App">
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={auth ? <Home /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!auth ? <Auth /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
