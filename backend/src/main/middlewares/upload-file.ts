9a57d9a8fc44611450ace239e6e428353ae52002
edusantanaw <102832370+edusantanaw@users.noreply.github.com>
2023-03-15 14:36:36 -0300

    feat: multer middleware config

---
+++ backend/src/main/middlewares/upload-file.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + ".jpg" || ".png" || ".wepg");
+  },
+});
+
+const upload = multer({ storage }).any();
+
+export const fileUpload = (req: Request, res: Response, next: NextFunction) => {
+  upload(req, res, (err) => {
+    if (err) console.log(err);
+    next(undefined);
+  });
+};
